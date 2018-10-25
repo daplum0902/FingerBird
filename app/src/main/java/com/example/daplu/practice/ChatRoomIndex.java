0e6b515bae9f92a76fa4c236e31ec5858108a896
alex112401 <alex112401@gmail.com>
2018-10-25 21:54:00 +0800

    BottomFragConnect

--- app/src/main/java/com/example/daplu/practice/ChatRoomIndex.java
+++ app/src/main/java/com/example/daplu/practice/ChatRoomIndex.java
@@ -1,5 +1,7 @@
 package com.example.daplu.practice;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
@@ -17,6 +19,32 @@ import java.util.List;
 import java.util.Map;
 
 public class ChatRoomIndex extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    // The request code must be 0 or greater.
+    private static final int PLUS_ONE_REQUEST_CODE = 0;
+    // The URL to +1.  Must be a valid URL.
+    private final String PLUS_ONE_URL = "http://developer.android.com";
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public ChatRoomIndex(){
+
+    }
+
+    public static MainPhome newInstance(String param1, String param2) {
+        MainPhome fragment = new MainPhome();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
         private Button btn;
         private View view;
@@ -59,9 +87,42 @@ public class ChatRoomIndex extends Fragment {
         }
 
 
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
 
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
 
-
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
 
 }
--- app/src/main/java/com/example/daplu/practice/MainActivity.java
+++ app/src/main/java/com/example/daplu/practice/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.daplu.practice;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
@@ -13,24 +18,134 @@ import android.view.WindowManager;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class MainActivity extends AppCompatActivity {
-
-    BottomNavigationViewEx bottomNavigationViewEx;
+public class MainActivity extends AppCompatActivity
+implements MainPhome.OnFragmentInteractionListener,ChatRoomIndex.OnFragmentInteractionListener
+{
+    private BottomNavigationViewEx bottomNavigationViewEx;
+    private MainPhome mainPhome;
+    private ChatRoomIndex chatRoomIndex;
+    private Fragment[] fragments;
+    private int lastfragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFragment();
+
+    }
+
+    private void initFragment() {
+        mainPhome = new MainPhome();
+        chatRoomIndex = new ChatRoomIndex();
+        fragments = new Fragment[]{mainPhome,mainPhome,mainPhome,chatRoomIndex,chatRoomIndex};
+        lastfragment = 0;
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainview,mainPhome).show(mainPhome).commit();
+
+
 
 
         bottomNavigationViewEx = (BottomNavigationViewEx)findViewById(R.id.BottomNavViewBar);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+
+        bottomNavigationViewEx.setOnNavigationItemSelectedListener(changeFragment);
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener changeFragment= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId())
+            {
+                case R.id.home:
+                {
+                    if(lastfragment!=0)
+                    {
+                        switchFragment(lastfragment,0);
+                        lastfragment=0;
+
+                    }
+
+                    return true;
+                }
+                case R.id.Post:
+                {
+                    if(lastfragment!=1)
+                    {
+                        switchFragment(lastfragment,1);
+                        lastfragment=1;
+
+                    }
+
+                    return true;
+                }
+                case R.id.Schedule:
+                {
+                    if(lastfragment!=2)
+                    {
+                        switchFragment(lastfragment,2);
+                        lastfragment=2;
+
+                    }
+
+                    return true;
+                }
+
+                case R.id.Chat:
+                {
+                    if(lastfragment!=3)
+                    {
+                        switchFragment(lastfragment,3);
+                        lastfragment=3;
+
+                    }
+
+                    return true;
+                }
+                case R.id.Notice:
+                {
+                    if(lastfragment!=4)
+                    {
+                        switchFragment(lastfragment,4);
+                        lastfragment=2;
+
+                    }
+
+                    return true;
+                }
+
+
+            }
+
+
+            return false;
+        }
+    };
+    //切换Fragment
+    private void switchFragment(int lastfragment,int index)
+    {
+        android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.hide(fragments[lastfragment]);//隐藏上个Fragment
+        if(fragments[index].isAdded()==false)
+        {
+            transaction.add(R.id.mainview,fragments[index]);
+
+
+        }
+        transaction.show(fragments[index]).commitAllowingStateLoss();
+
 
     }
 
     private void setupBottomNavigationView(){
 
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
 
