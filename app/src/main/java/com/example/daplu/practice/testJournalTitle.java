fa74e78ea68dbd7f051b3165302da994dc036317
daplum0902 <daplum0902@gmail.com>
2018-10-28 13:44:49 +0800

    徵伴貼文及遊記介面完工

---
+++ app/src/main/java/com/example/daplu/practice/testJournalTitle.java
@@ -0,0 +1,17 @@
+package com.example.daplu.practice;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class testJournalTitle extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.test_journal_detail_title, container, false);
+
+        return view;
+    }
+}
